27b14359360878a72c6fe42e15dea0c4ecf5488f
Matheus Rennan <134556078+orea147@users.noreply.github.com>
2024-03-01 14:51:11 -0300

    leitura do arquivo cnf

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+def ler_cnf(cnf):
+    with open(cnf, 'r') as file:
+        content = file.readlines()
+    F = []
+    for line in content:
+        if line.startswith('c') or line.startswith('p'):
+            continue
+        clausula = list(map(int, line.split()[:-1]))
+        F.append(clausula)
+    return F
+
 def main():
     F = ler_cnf('aim-100-2_0-no-1.cnf') # Insira o nome do arquivo cnf
     result = DPLL(F)
