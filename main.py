53632f93399774f2ed552a7dc3e91485bd227883
Matheus Rennan <134556078+orea147@users.noreply.github.com>
2024-03-06 12:56:10 -0300

    funcao simplifica

--- main.py
+++ main.py
@@ -9,6 +9,22 @@ def ler_cnf(cnf):
         F.append(clausula)
     return F
 
+def simplifica(F):
+    while True:
+        encontrou_clausula_unitaria = False
+        for c in F:
+            if len(c) == 1:  
+                encontrou_clausula_unitaria = True
+                unitaria = c[0]
+                break
+        if not encontrou_clausula_unitaria: 
+            break
+        F = [c for c in F if unitaria not in c]  
+        for c in F:  
+            if -unitaria in c:
+                c.remove(-unitaria)
+    return F
+
 def main():
     F = ler_cnf('aim-100-2_0-no-1.cnf') # Insira o nome do arquivo cnf
     result = DPLL(F)
